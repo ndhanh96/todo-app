a2d0cdb1f7c36da4550214b1e879d5f3c684fa03
Nguyen Hanh <ndhanh96@gmail.com>
2022-03-24 18:53:59 +0700

    basic functions: add, delete, update, save.

---
+++ components/TodoForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useState, useRef, useEffect } from 'react';
+
+function TodoForm({ todo, deleteTodo, updateTodo }) {
+  const [editForm, setEditForm] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState(todo);
+  // const originalTodo = useRef('');
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setCurrentTodo(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateTodo(todo, currentTodo);
+    setEditForm(!editForm);
+    console.log('submit');
+  };
+
+  return (
+    <>
+      <li>
+        <form onSubmit={handleSubmit}>
+          <input
+            disabled={!editForm}
+            className='border-2 border-yellow-400 outline-none disabled:bg-red-500 '
+            value={currentTodo}
+            onChange={(e) => onChange(e)}
+          />
+          <span>
+            <button
+              disabled={editForm}
+              type='button'
+              className='p-2 mx-1 bg-red-600 rounded-lg disabled:bg-slate-500'
+              onClick={() => deleteTodo(todo)}
+            >
+              Delete todo
+            </button>
+            <button
+              disabled={editForm}
+              type='button'
+              className='p-2 mx-1 bg-green-600 rounded-lg disabled:bg-slate-500'
+              onClick={() => setEditForm(!editForm)}
+            >
+              Update
+            </button>
+            <button
+              disabled={!editForm}
+              className='p-2 bg-green-600 disabled:bg-slate-500 rounded-lg'
+              type='submit'
+            >
+              Save
+            </button>
+          </span>
+        </form>
+      </li>
+    </>
+  );
+}
+
+export default TodoForm;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState, useEffect, useRef } from 'react';
+import TodoForm from '../components/TodoForm';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(['test']);
   const filledInput = useRef('');
 
   const deleteTodo = (deleteTodo) => {
@@ -13,10 +14,20 @@ export default function Home() {
       return todo != deleteTodo;
     });
     setTodos(newTodos);
+    filledInput.current.value = '';
   };
 
   const addTodo = (addedTodo) => {
     setTodos([...todos, addedTodo]);
+    filledInput.current.value = '';
+  };
+
+  const updateTodo = (oldTodo, updatedTodo) => {
+    console.log(updatedTodo);
+    const updatedTodos = todos.map((todo) => {
+      return todo == oldTodo ? (todo = updatedTodo) : todo;
+    });
+    setTodos(updatedTodos);
   };
 
   const handleSubmit = (e) => {
@@ -32,10 +43,30 @@ export default function Home() {
     <div>
       <h1>Todo app</h1>
       <form onSubmit={handleSubmit}>
-        <input ref={filledInput} placeholder='' />
-        <button onClick={() => addTodo(filledInput.current.value)} type='submit'>Add</button>
-        <button onClick={() => deleteTodo(filledInput.current.value)}>Delete todo</button>
+        <input
+          className='border border-yellow-500 focus:border-yellow-800 outline-none'
+          ref={filledInput}
+          placeholder=''
+        />
+        <button
+          className='p-2 mx-1 bg-slate-600 rounded-lg '
+          onClick={() => addTodo(filledInput.current.value)}
+          type='submit'
+        >
+          Add
+        </button>
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <TodoForm
+              todo={todo}
+              deleteTodo={deleteTodo}
+              updateTodo={updateTodo}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
