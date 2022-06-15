3c55bc073b0dd8cda6cba06b3141906daeb092ad
Nguyen Hanh <ndhanh96@gmail.com>
2022-06-15 16:42:25 +0700

    style layout and html

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,7 @@ function Layout({ children } : {children: React.ReactNode}) {
   return (
     <div
       id='layout'
-      className='flex m-2 sm:mx-12 lg:mx-20 2xl:mx-52 2xl:my-10 py-2 rounded-md flex-wrap justify-center md:text-2xl 2xl:text-4xl bg-yellow-200 border-2 border-orange-700 shadow-xl shadow-orange-700/70'
+      className='flex m-2 sm:mx-12 lg:mx-20 2xl:mx-52 my-4 lg:my-8 2xl:my-20 py-2 rounded-md flex-wrap justify-center md:text-2xl 2xl:text-4xl bg-yellow-200 border-2 border-orange-700 shadow-2xl shadow-orange-800/80'
     >
       <Head>
         <title>Todo things ....</title>
